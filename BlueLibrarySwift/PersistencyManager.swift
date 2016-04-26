dbc597aab8531d962e4795909803ae45261f1bbd
rafael zhou <2008zkapie@163.com>
2016-04-26 13:06:46 +0800

    归档Archiving

--- BlueLibrarySwift/PersistencyManager.swift
+++ BlueLibrarySwift/PersistencyManager.swift
@@ -14,39 +14,52 @@ class PersistencyManager: NSObject {
     private var albums = [Album]()
     
     override init() {
+        super.init()
+        if let data = NSData(contentsOfFile: NSHomeDirectory().stringByAppendingString("/Documents/albums.bin")) {
+            let unarchiveAlbums = NSKeyedUnarchiver.unarchiveObjectWithData(data) as! [Album]
+            if let unwrappedAlbum : [Album]  = unarchiveAlbums {
+                albums = unwrappedAlbum
+            }
+        } else {
+            createPlaceholderAlbum()
+        }
+    }
+    
+    func createPlaceholderAlbum() {
         //Dummy list of albums
         let album1 = Album(title: "Best of Bowie",
                            artist: "David Bowie",
                            genre: "Pop",
-                           coverUrl: "http://img3.douban.com/mpic/s1497881.jpg",
+                           coverUrl: "http://www.coversproject.com/static/thumbs/album/album_david%20bowie_best%20of%20bowie.png",
                            year: "1992")
         
         let album2 = Album(title: "It's My Life",
                            artist: "No Doubt",
                            genre: "Pop",
-                           coverUrl: "http://img3.doubanio.com/mpic/s3880529.jpg",
+                           coverUrl: "http://www.coversproject.com/static/thumbs/album/album_no%20doubt_its%20my%20life%20%20bathwater.png",
                            year: "2003")
         
         let album3 = Album(title: "Nothing Like The Sun",
                            artist: "Sting",
                            genre: "Pop",
-                           coverUrl: "http://img3.doubanio.com/mpic/s3708339.jpg",
+                           coverUrl: "http://www.coversproject.com/static/thumbs/album/album_sting_nothing%20like%20the%20sun.png",
                            year: "1999")
         
         let album4 = Album(title: "Staring at the Sun",
                            artist: "U2",
                            genre: "Pop",
-                           coverUrl: "http://img3.douban.com/mpic/s1882422.jpg",
+                           coverUrl: "http://www.coversproject.com/static/thumbs/album/album_u2_staring%20at%20the%20sun.png",
                            year: "2000")
         
         let album5 = Album(title: "American Pie",
                            artist: "Madonna",
                            genre: "Pop",
-                           coverUrl: "http://img3.douban.com/mpic/s3105351.jpg",
+                           coverUrl: "http://www.coversproject.com/static/thumbs/album/album_madonna_american%20pie.png",
                            year: "2000")
-        
         albums = [album1, album2, album3, album4, album5]
+        saveAlbums()
     }
+
     
     
     func getAlbums() -> [Album] {
@@ -64,6 +77,13 @@ class PersistencyManager: NSObject {
     func deleteAlbumAtIndex(index: Int) {
         albums.removeAtIndex(index)
     }
+    
+    func saveAlbums() {
+        let filename = NSHomeDirectory().stringByAppendingString("/Documents/albums.bin")
+        let data = NSKeyedArchiver.archivedDataWithRootObject(albums)
+        data.writeToFile(filename, atomically: true)
+    }
+    
     
     func saveImage(image: UIImage, filename: String) {
         let path = NSHomeDirectory().stringByAppendingString("/Documents/\(filename)")
